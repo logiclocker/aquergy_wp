06efeb1ff5e41798dd82c01d4331fcee5f25dd3e
darshakthakore <darshakthakore@gmail.com>
2019-06-21 17:37:29 +0000

    updating the site path and clearing out temp passwords

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'divsizvx_aquergy' );
+define( 'DB_NAME', 'blank' );
 
 /** MySQL database username */
-define( 'DB_USER', 'divsizvx_aquergy' );
+define( 'DB_USER', 'blank' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'aquergy123' );
+define( 'DB_PASSWORD', 'blank' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
